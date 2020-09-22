bf3aa7e00f0e49a79af48e2c340f2b87efc4157e
zhenghao01 <zhenghao01@shengqugames.com>
2020-09-22 20:20:16 +0800

    修改

--- code/1/server.js
+++ code/1/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/html/"));
+
+app.get("/1",(req,res)=>{
+    res.send("OK");
+})
 app.listen("3000", () => {
     console.log("服务已经开启")
 })
